57e712a65771373adc3338ea5d667a4aba57f46f
GFmatrix <sketchapps.lp@gmail.com>
2025-02-15 22:11:10 +0500

    Prod commit

--- backend/main.py
+++ backend/main.py
@@ -5,12 +5,13 @@ from starlette.middleware.cors import CORSMiddleware
 import argparse
 
 app = FastAPI(
-    title="Unit Of Work & FastAPI Users"
+    title="Unit Of Work & FastAPI Users",
+    root_path="/api"
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3331"],  # "*" allows all origins
+    allow_origins=["http://localhost:3000", "http://localhost:3331", "https://hydrologic-database.uz/"],  # "*" allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  devServer: {
+    port: 8199,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'hydrologic-database.uz',
+      '.hydrologic-database.uz',
+      'localhost'
+    ],
+    client: {
+      webSocketURL: {
+        hostname: 'localhost',
+        port: 8199
+      }
+    }
+  },
+  
+  // Optional: Configure public path if needed
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://hydrologic-database.uz/'
+    : '/'
+}
